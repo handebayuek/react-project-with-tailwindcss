36030d2cc3b5b1735851c7218a9c51bbbb56a72d
handebayuek <hande.bayuek@gmail.com>
2025-02-11 11:50:10 +0100

    deployed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: './',
+  base: '/react-project-with-tailwindcss/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
